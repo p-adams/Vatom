5621fa2bb110778a6a0781f3d1e58179c6b466ad
p-adams <paadams@nmu.edu>
2018-05-04 00:07:53 -0500

    add vue dep

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-exports.Vatom = function(){
-    console.log("Testing")
-}
+const Vue = require("vue");
+
+// Scalar or compound value -> Vue instance
+
+const createAtom = ({ value }) => {
+  return new Vue({
+    data: {
+      value
+    },
+    methods: {
+      swap(currentValue, fn) {
+        fn.apply(currentValue);
+      },
+      reset(atom, newValue) {}
+    },
+    computed: {
+      deref() {
+        return atom => {};
+      }
+    }
+  });
+};
+
+console.log(createAtom({ value: { key: 1, value: "meow" } }).$data.value.key);
+
+exports = {
+  createAtom
+};
