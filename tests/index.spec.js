4025176231f9ab4cec1ae091f35c47470418cc28
p-adams <paadams@nmu.edu>
2018-05-05 23:34:48 -0500

    add check to determine if array contains objects

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,10 @@
-const { isSupportedValueType } = require("../index");
+const { convertArrayToObject, isSupportedValueType } = require("../index");
+
+const users = [
+  { id: 1, username: "foo" },
+  { id: 2, username: "bar" },
+  { id: 3, username: "baz" }
+];
 
 test("isSupportedValueType is false when passed string", () => {
   expect(isSupportedValueType("meow")).toBeFalsy();
@@ -16,6 +22,16 @@ test("isSupportedValueType is true when passed Object", () => {
   expect(isSupportedValueType({ key: 1, value: "foo" })).toBeTruthy();
 });
 
-test("isSupportedValueType is true when passed Array", () => {
-  expect(isSupportedValueType(["foo", 1, "baz"])).toBeTruthy();
+test("isSupportedValueType is falsy when passed Array that contains scalar values", () => {
+  expect(
+    isSupportedValueType(["foo", { id: 1, name: "bar" }, "baz"])
+  ).toBeFalsy();
+});
+
+test("isSupportedValueType is truthy when passed Array that contains object", () => {
+  expect(isSupportedValueType(users)).toBeTruthy();
+});
+
+test("convertArrayToObject correctly converts array to object", () => {
+  expect(convertArrayToObject(users, "id")).toBeInstanceOf(Object);
 });
