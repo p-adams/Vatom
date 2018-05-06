01da97538536a7a5ee85827fe83e0e8e5f0acc81
p-adams <paadams@nmu.edu>
2018-05-06 03:39:33 -0500

    make atom more flexible

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
 const Vue = require("vue");
 
-module.exports.isSupportedValueType = ro => {
-  const isObject = obj => Object.getPrototypeOf(obj) === Object.prototype;
-  // if reference object is an array
-  // make sure that it is an array of objects
-  let arrayContainsObject;
-  if (Array.isArray(ro)) {
-    arrayContainsObject =
-      ro.findIndex(obj => !isObject(obj)) > -1 ? false : true;
-  }
-  return arrayContainsObject || isObject(ro);
-};
-
-module.exports.convertArrayToObject = (array, key) =>
-  array.reduce((obj, item) => {
-    obj[item[key]] = item;
-    return obj;
-  }, {});
-
-// Object | Array -> Vue instance
-const Atom = referenceObject => {
-  if (!isSupportedValueType(referenceObject)) {
-    console.warn(`${referenceObject} must be an object or array of objects`);
-    return;
-  }
-  if (Array.isArray(referenceObject))
-    referenceObject = convertArrayToObject(referenceObject);
-
+// Value -> Vue instance
+module.exports.Atom = value => {
   return new Vue({
     data: {
-      ro: referenceObject
+      value
     },
     methods: {
-      // swap: Function -> Object
-      swap(updateFunction) {
-        updateFunction.apply(this.ro);
+      // swap: Function -> value
+      swap(updateFunction, args) {
+        this.value = updateFunction.apply(null, args);
       },
-      // reset: Object -> Object
+      // reset: value -> value
       reset(newValue) {}
     },
     computed: {
       deref() {
-        return this.ro;
+        return this.value;
       }
     }
   });
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,37 +1,6 @@
-const { convertArrayToObject, isSupportedValueType } = require("../index");
+const { Atom } = require("../index");
 
-const users = [
-  { id: 1, username: "foo" },
-  { id: 2, username: "bar" },
-  { id: 3, username: "baz" }
-];
-
-test("isSupportedValueType is false when passed string", () => {
-  expect(isSupportedValueType("meow")).toBeFalsy();
-});
-
-test("isSupportedValueType is false when passed number", () => {
-  expect(isSupportedValueType(1)).toBeFalsy();
-});
-
-test("isSupportedValueType is false when passed boolean", () => {
-  expect(isSupportedValueType("meow")).toBeFalsy();
-});
-
-test("isSupportedValueType is true when passed Object", () => {
-  expect(isSupportedValueType({ key: 1, value: "foo" })).toBeTruthy();
-});
-
-test("isSupportedValueType is falsy when passed Array that contains scalar values", () => {
-  expect(
-    isSupportedValueType(["foo", { id: 1, name: "bar" }, "baz"])
-  ).toBeFalsy();
-});
-
-test("isSupportedValueType is truthy when passed Array that contains object", () => {
-  expect(isSupportedValueType(users)).toBeTruthy();
-});
-
-test("convertArrayToObject correctly converts array to object", () => {
-  expect(convertArrayToObject(users, "id")).toBeInstanceOf(Object);
+test("Dereferencing Atom's reference object takes correct snapshot of current reference object", () => {
+  const count = new Atom(1);
+  expect(count.deref()).toBe(1);
 });
